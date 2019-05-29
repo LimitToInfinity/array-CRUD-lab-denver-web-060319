efb8995602eec8011ff28abf903f6e53a0d96efa
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-29 19:01:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["oranges", "I", "bananas", "pineapples"]
+  array = ["oranges", "am", "bananas", "pineapples"]
 end
 
 def add_element_to_end_of_array(array, element)
